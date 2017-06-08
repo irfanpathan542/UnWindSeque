06b11847087dbcc7084eee2edc1515bbea268f67
iFlame <iflame@Administrators-iMac.local>
2017-06-08 09:15:31 +0530

    Initial Commit

---
+++ unwindSegue/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  unwindSegue
+//
+//  Created by iFlame on 5/14/17.
+//  Copyright © 2017 iFlame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var btn: UIButton!
+    
+    
+    @IBOutlet var img: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func seque(seque: UIStoryboardSegue){
+        
+    }
+
+
+}
+
